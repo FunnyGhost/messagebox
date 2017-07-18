e788c86875ff8834ea62e44cd06ceb4ffa530821
Catalin Ciubotaru <catalinciubot@gmail.com>
2017-07-19 01:12:28 +0200

    More work on the design

--- src/app/messages/redux/reducer.ts
+++ src/app/messages/redux/reducer.ts
@@ -3,7 +3,42 @@ import { ADD_MESSAGE } from './actions';
 
 import { Message } from '../models/message.model';
 
-export function messagesReducer(state: Message[] = [], action: Action) {
+const initialState: Message[] = [{
+  name: 'Catalin',
+  text: 'This is an awesome message'
+}, {
+  name: 'Andra',
+  text: 'I have an awesome phone'
+}, {
+  name: 'Pixie',
+  text: 'All these fleas are killing me!'
+}, {
+  name: 'Koda',
+  text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry'
+}, {
+  name: 'Valentin',
+  text: 'What\'s happening here?'
+}, {
+  name: 'Valentin',
+  text: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout'
+}, {
+  name: 'Valentin',
+  text: 'What\'s happening here?'
+}, {
+  name: 'Valentin',
+  text: 'What\'s happening here?'
+}, {
+  name: 'Valentin',
+  text: 'What\'s happening here?'
+}, {
+  name: 'Valentin',
+  text: 'What\'s happening here?'
+}, {
+  name: 'Valentin',
+  text: 'What\'s happening here?'
+}];
+
+export function messagesReducer(state: Message[] = initialState, action: Action) {
   switch (action.type) {
     case ADD_MESSAGE:
       return [...state, action.payload as Message];
